3b5d0b3b57bc6598d683465d2c55584797dfc182
furiko <furiko522code@gmail.com>
2022-09-30 06:30:44 +0900

    :recycle: add chakra-ui

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { NextPage } from 'next'
+import { Box, Button, Container } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello</h1>
+    // 640以下だとsm, 640より大きいとmd, 1024より大きいとlg, 1280より大きいとxl。ただし、marginが考慮されない
+    <Container maxW={{ base: 'container.sm', md: 'container.md', lg: 'container.lg', xl: 'container.xl' }} bg="brand.900" p={0}>
+      <Box color="brand.900" bg="brand.100">Hello World!</Box>
+      <Button size='xl' variant='with-shadow'>
+        Welcome
+      </Button>
+    </Container>
   )
 }
 
