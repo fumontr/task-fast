4f064643a797d936415381d667668276a1183946
furiko <furiko522code@gmail.com>
2022-10-02 18:33:37 +0900

    :recycle: update test

--- app/src/lib/__tests__/util.spec.ts
+++ app/src/lib/__tests__/util.spec.ts
@@ -1,8 +1,10 @@
 import { getTodayForHeader } from '../util'
+import dayjs from 'dayjs'
 
 describe('ヘッダー', () => {
   test('ヘッダー用の日付を取得', () => {
     const today = getTodayForHeader()
-    expect(today).toEqual('2022/10/01 土')
+    const target = dayjs().locale('ja').format('YYYY/MM/DD ddd')
+    expect(today).toEqual(target)
   })
 })
