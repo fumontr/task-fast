3df657d2f47c2b935af8f16f2342c64387813d93
furiko <furiko522code@gmail.com>
2022-10-07 18:36:44 +0900

    :recycle: format

--- app/src/components/Task.tsx
+++ app/src/components/Task.tsx
@@ -7,27 +7,27 @@ export const Task = ({ task }: { task: string }) => {
   const bgDefault = 'brand.taskBackground'
   return (
     <Link as={NextLink} href="/taskDetail">
-    <Flex
-      h={8}
-      bg="brand.taskBackground"
-      alignItems="center"
-      justifyContent="space-between"
-    >
-      <Flex alignItems="center">
-        <IconButton
-          aria-label="start-task-icon"
-          icon={<VscDebugStart />}
-          w={8}
-          h={8}
-          bg={bgDefault}
-        />
-        <Text fontSize="12px">{task}</Text>
+      <Flex
+        h={8}
+        bg="brand.taskBackground"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex alignItems="center">
+          <IconButton
+            aria-label="start-task-icon"
+            icon={<VscDebugStart />}
+            w={8}
+            h={8}
+            bg={bgDefault}
+          />
+          <Text fontSize="12px">{task}</Text>
+        </Flex>
+        <Flex direction="column" pr={2}>
+          <Text fontSize={fontSize}>00:20</Text>
+          <Text fontSize={fontSize}>12:00</Text>
+        </Flex>
       </Flex>
-      <Flex direction="column" pr={2}>
-        <Text fontSize={fontSize}>00:20</Text>
-        <Text fontSize={fontSize}>12:00</Text>
-      </Flex>
-    </Flex>
     </Link>
   )
 }
