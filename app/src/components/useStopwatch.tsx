bb7eb554d1cdb8bedec080d61df9a1f9d9d2439b
furiko <38683013+furiko@users.noreply.github.com>
2023-05-13 20:58:22 +0900

    再開を正しくできるようにした

--- app/src/components/useStopwatch.tsx
+++ app/src/components/useStopwatch.tsx
@@ -5,23 +5,29 @@ export const useStopwatch = () => {
   const [isRunning, setIsRunning] = useState(false)
   const [startAt, setStartAt] = useState(dayjs())
   const [elapseTime, setElapseTime] = useState<number>(0)
+  const [stoppedElapsedTime, setStoppedElapsedTime] = useState<number>(0)
 
   const startStopwatch = useCallback(() => {
     const now = dayjs()
     setIsRunning(true)
     setStartAt(now)
-  }, [])
+    setElapseTime(stoppedElapsedTime)
+  }, [stoppedElapsedTime])
 
-  const stopStopwatch = useCallback(() => setIsRunning(false), [])
+  const stopStopwatch = useCallback(() => {
+    setIsRunning(false)
+    setStoppedElapsedTime(elapseTime)
+  }, [elapseTime])
 
   const resetStopwatch = useCallback(() => {
     setIsRunning(false)
     setElapseTime(0)
+    setStoppedElapsedTime(0)
   }, [])
 
   useEffect(() => {
     const timer = setInterval(
-      () => isRunning && setElapseTime(dayjs().diff(startAt, 'second', false)),
+      () => isRunning && setElapseTime(prev => prev + 1),
       1000
     )
     return () => clearInterval(timer)
